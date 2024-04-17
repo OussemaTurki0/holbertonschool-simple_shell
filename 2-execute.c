4bed134cbcf0dd97b4dc33600816fafe3a8039aa
ghofranaaa <smailikhaled841@gmail.com>
2024-04-17 16:43:04 +0100

    fixing the mixed declaration

--- 2-execute.c
+++ 2-execute.c
@@ -1,26 +1,35 @@
 #include "4-shell.h"
+/**
+ * execute_command - Execute a command.
+ * @args: Array of command arguments.
+ *
+ * Return: 1 to continue the shell loop, 0 to exit the shell.
+ */
 
 int execute_command(char **args)
 {
+	pid_t pid;
+
 	/* Check if the command is empty */
 	if (args[0] == NULL)
 	{
-		return 1; /* Return 1 to continue the shell loop */
+		return (1); /* Return 1 to continue the shell loop */
 	}
 
 	/* Check if the command is the exit command */
 	if (str(args[0], "exit") == 0)
 	{
-		return 0; /* Return 0 to exit the shell */
+		return (0); /* Return 0 to exit the shell */
 	}
 
 	/* Fork a new process */
-	pid_t pid = fork();
+	pid = fork();
+
 	if (pid == 0)
 	{
 		/* Child process */
 		/* Execute the command */
-		if (exe(args[0], args) == -1)
+		if (execve(args[0], args) == -1)
 		{
 			error("shell");
 		}
@@ -37,6 +46,5 @@ int execute_command(char **args)
 		/* Wait for the child process to finish */
 		wait(NULL);
 	}
-
-	return 1; /* Return 1 to continue the shell loop */
+	return (1); /* Return 1 to continue the shell loop */
 }
