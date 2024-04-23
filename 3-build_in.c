c0eb0fe605b8ed3cd03210003f6a5671c702cd19
oussematurki0 <oussematurki0@gmail.com>
2024-04-23 11:17:46 +0200

    commit

--- 3-build_in.c
+++ 3-build_in.c
@@ -10,8 +10,7 @@
  */
 int shell_exit(__attribute__((unused)) char **args)
 {
-
-	return (0); /* Return 0 to exit the shell */
+    return (0); /* Return 0 to exit the shell */
 }
 
 /**
@@ -21,14 +20,15 @@ int shell_exit(__attribute__((unused)) char **args)
  */
 int shell_env(__attribute__((unused)) char **args)
 {
-	char **env;
-	/* Print the current envir */
-	for (env = environ; *env != NULL; env++)
-	{
-		printf("%s\n", *env);
-	}
-	return (1); /* Return 1 to continue the shell loop */
+    char **env;
+    /* Print the current environment */
+    for (env = environ; *env != NULL; env++)
+    {
+        printf("%s\n", *env);
+    }
+    return (1); /* Return 1 to continue the shell loop */
 }
+
 /**
  * shell_cd - Change the current working directory.
  * @args: Array of command arguments
@@ -37,26 +37,25 @@ int shell_env(__attribute__((unused)) char **args)
  */
 int shell_cd(char **args)
 {
-	if (args[1] == NULL)
-	{
-		fprintf(stderr, "Usage: cd <directory>\n");
-		return (-1);
-	}
-	if (chdir(args[1]) != 0)
-	{
-		perror("cd");
-		return (-1);
-	}
-	return (1);
+    if (args[1] == NULL)
+    {
+        fprintf(stderr, "Usage: cd <directory>\n");
+        return (-1);
+    }
+    if (chdir(args[1]) != 0)
+    {
+        perror("cd");
+        return (-1);
+    }
+    return (1);
 }
 
 /**
  * handle_built_in - Handles built-in commands.
  * @args: Array of command arg.
- * @status: Pointer to the status variable.
  * Return: 1 if handled as built-in, 0 otherwise.
  */
-int handle_built_in(char **args, int *status)
+int handle_built_in(char **args)
 {
     buildin cmds[] = {
         {"exit", shell_exit},
