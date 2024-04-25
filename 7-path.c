f0145fa3b5e08f9870cff0a4cf34c2315a8d6aee
ghofranaaa <smailikhaled841@gmail.com>
2024-04-25 21:31:57 +0100

    getting the first path

--- 7-path.c
+++ 7-path.c
@@ -2,46 +2,24 @@
 
 /**
  * get_command_path - Retrieve the full path of a command.
- * @cmd: The command name.
+ * @command: The command name.
  *
  * Return: The full path of the command, or NULL if not found.
  */
-char *get_command_path(const char *cmd)
-{
-char *full_path;
-char *path;
-char *token;
-char *temporary;
-char *delim = ":";
-size_t len;
 
-if (cmd == NULL)
-return (NULL);
-path = getenv("PATH");     /* Get the PATH environment variable */
-if (path == NULL)
-return (NULL);
-temporary = strdup(path);     /* Create a copy of PATH to tokenize */
-if (temporary == NULL)
-return (NULL);
-/* Tokenize the PATH variable to search for the command */
-token = strtok(temporary, delim); /* Tokenize the PATH variable */
-while (token != NULL)
+char *get_command_path(const char *command)
 {
-len = strlen(token) + strlen(cmd) + 2; /* Construct the full path */
-full_path = malloc(len);
-if (full_path == NULL)
-{
-free(temporary);
-return (NULL);
-}
-if (access(full_path, F_OK) == 0) /* Check the command exists at the path */
-{
-free(temporary);
-return (full_path);
-}
-free(full_path);
-token = strtok(NULL, delim);
-}
-free(temporary);
-return (NULL); /* Command not found */
+char *gpath; /* Pointer to store each path directory */
+char *envr; /* Pointer to store the value of the PATH environment variable */
+char *cmmd; /* Pointer to store the constructed command path */
+char delim[] = ":"; /* Delimiter used to separate path directories */
+
+	if (command) /* Check if command is not NULL */
+	/* Return command if it exists */
+		return (access(command, F_OK) == 0 ? strdup(command) : NULL);
+
+	envr = _getenv("PATH");
+	if (!envr) /* If PATH is not set, return NULL */
+		return (NULL);
+	gpath = strtok(envr, delim); /* Get the first path directory */
 }
