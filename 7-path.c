7d295bd748228130fdc483614b66a9618900bf35
ghofranaaa <smailikhaled841@gmail.com>
2024-04-25 11:08:12 +0100

    betty coding style

--- 7-path.c
+++ 7-path.c
@@ -8,53 +8,40 @@
  */
 char *get_command_path(const char *cmd)
 {
-	char *full_path;
-    char *path;
-    char *token;
-    char *temporary;
-    char *delim = ":";
-	size_t len;
+char *full_path;
+char *path;
+char *token;
+char *temporary;
+char *delim = ":";
+size_t len;
 
-    if (cmd == NULL)
-        return (NULL);
-
-    /* Get the PATH environment variable */
-    path = getenv("PATH");
-    if (path == NULL)
-        return (NULL);
-
-    /* Create a copy of PATH to tokenize */
-    temporary = strdup(path);
-    if (temporary == NULL)
-        return (NULL);
-
-    /* Tokenize the PATH variable to search for the command */
-    token = strtok(temporary, delim);
-    while (token != NULL)
-    {
-        /* Construct the full path to the command */
-		len = strlen(token) + strlen(cmd) + 2;
-        full_path = malloc(len);
-        if (full_path == NULL)
-        {
-            free(temporary);
-            return (NULL);
-        }
-        snprintf(full_path, len, "%s/%s", token, cmd);
-    	printf("Trying path: %s\n", full_path);
-
-
-        /* Check if the command exists at the constructed path */
-        if (access(full_path, F_OK) == 0)
-        {
-            free(temporary);
-            return (full_path);
-        }
-
-        free(full_path);
-        token = strtok(NULL, delim);
-    }
-
-    free(temporary);
-    return (NULL); /* Command not found */
+if (cmd == NULL)
+return (NULL);
+path = getenv("PATH");     /* Get the PATH environment variable */
+if (path == NULL)
+return (NULL);
+temporary = strdup(path);     /* Create a copy of PATH to tokenize */
+if (temporary == NULL)
+return (NULL);
+/* Tokenize the PATH variable to search for the command */
+token = strtok(temporary, delim); /* Tokenize the PATH variable */
+while (token != NULL)
+{
+len = strlen(token) + strlen(cmd) + 2; /* Construct the full path */
+full_path = malloc(len);
+if (full_path == NULL)
+{
+free(temporary);
+return (NULL);
+}
+if (access(full_path, F_OK) == 0) /* Check the command exists at the path */
+{
+free(temporary);
+return (full_path);
+}
+free(full_path);
+token = strtok(NULL, delim);
+}
+free(temporary);
+return (NULL); /* Command not found */
 }
