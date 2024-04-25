ce92aa41ea55c34c0b761566a5bd87fc2620197d
ghofranaaa <smailikhaled841@gmail.com>
2024-04-25 21:37:50 +0100

    allocating memory for the cmd

--- 7-path.c
+++ 7-path.c
@@ -1,22 +1,22 @@
 #include "4-shell.h"
 
 /**
-* _getenv - handling the environment.
-* @env_var: the variable to retrieve.
-* Return: value of the variable, otherwise NULL.
-*/
+ * _getenv - handling the environment.
+ * @env_var: the variable to retrieve.
+ * Return: value of the variable, otherwise NULL.
+ */
 char *_getenv(const char *env_var)
 {
-int i = 0;
-char *key;
-while (environ[i])
-{
-key = strtok(environ[i], "="); /* Extract key from environment variable */
-if (strcmp(env_var, key) == 0)
-return (strtok(NULL, "/")); /* Return value if key matches */
-i++; /* Move to the next environment variable */
-}
-return (NULL); /* Return NULL if no match found */
+	int i = 0;
+	char *key;
+	while (environ[i])
+	{
+		key = strtok(environ[i], "="); /* Extract key from environment variable */
+		if (strcmp(env_var, key) == 0)
+			return (strtok(NULL, "/")); /* Return value if key matches */
+		i++;							/* Move to the next environment variable */
+	}
+	return (NULL); /* Return NULL if no match found */
 }
 
 /**
@@ -28,17 +28,26 @@ return (NULL); /* Return NULL if no match found */
 
 char *get_command_path(const char *command)
 {
-char *gpath; /* Pointer to store each path directory */
-char *envr; /* Pointer to store the value of the PATH environment variable */
-char *cmmd; /* Pointer to store the constructed command path */
-char delim[] = ":"; /* Delimiter used to separate path directories */
+	char *gpath;		/* Pointer to store each path directory */
+	char *envr;		/* Pointer to store the value of the PATH environment variable */
+	char *cmmd;			/* Pointer to store the constructed command path */
+	char delim[] = ":"; /* Delimiter used to separate path directories */
 
 	if (command) /* Check if command is not NULL */
-	/* Return command if it exists */
+				 /* Return command if it exists */
 		return (access(command, F_OK) == 0 ? strdup(command) : NULL);
 
 	envr = _getenv("PATH");
 	if (!envr) /* If PATH is not set, return NULL */
 		return (NULL);
 	gpath = strtok(envr, delim); /* Get the first path directory */
+	while (gpath)	 /* Loop through each path directory */
+	{
+		cmmd = malloc(strlen(gpath) + strlen(command) + 2);
+		if (!cmmd) /* Check if memory allocation failed */
+		{
+			free(envr);	   /* Free memory allocated for envr */
+			return (NULL); /* Return NULL */
+		}
+	}
 }
